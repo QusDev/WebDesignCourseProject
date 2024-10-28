f1c5e7f12a839c9d716ee5cfe51a666223432aad
QusDev <qusdeveloper@gmail.com>
2024-10-28 17:23:53 +0200

    stylization testimonials section

---
+++ js/swiper-testimonials.js
@@ -0,0 +1,9 @@
+const testimonials_swiper = new Swiper('.testimonials-slider', {
+    slidesPerView: 1.25,
+    spaceBetween: 104,
+
+    navigation: {
+        nextEl: '.testimonials-slider-button-next',
+        prevEl: '.testimonials-slider-button-prev',
+    }
+});

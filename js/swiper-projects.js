b7b6d08482d74b808dfde7fb97e31e5f727aa80a
QusDev <qusdeveloper@gmail.com>
2024-10-28 15:06:04 +0200

    stylization our projects section

---
+++ js/swiper-projects.js
@@ -0,0 +1,9 @@
+const project_swiper = new Swiper('.our-projects-slider', {
+    slidesPerView: 2,
+    spaceBetween: 64,
+
+    navigation: {
+        nextEl: '.our-project-slider-button-next',
+        prevEl: '.our-project-slider-button-prev',
+    }
+});

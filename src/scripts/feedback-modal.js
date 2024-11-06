a07d3a7836357f8126fe4a20396bbd698c392998
QusDev <qusdeveloper@gmail.com>
2024-11-06 19:58:06 +0200

    add adaptive, validation, clearing to feedback form

--- src/scripts/feedback-modal.js
+++ src/scripts/feedback-modal.js
@@ -8,34 +8,116 @@ const refs = {
   modal: document.querySelector("[data-feedback-modal]"),
 };
 
-refs.openModalBtns.forEach(btn => {
-  btn.addEventListener("click", toggleModal);
-})
-refs.closeModalBtn.addEventListener("click", toggleModal);
+const formInputs = {
+  name: document.getElementById("feedback-name"),
+  email: document.getElementById("feedback-mail"),
+  message: document.getElementById("feedback-comment"),
+  status: document.getElementById("feedback-status"),
+};
 
-function toggleModal() {
-  refs.modal.classList.toggle("hidden");
+function clearFormInputs() {
+  formInputs.name.value = "";
+  formInputs.email.value = "";
+  formInputs.message.value = "";
+  formInputs.status.textContent = "";
+}
+
+function formOpenCloseListeners() {
+  const toggleModal = () => {
+    refs.modal.classList.toggle("hidden");
+    clearFormInputs();
+  };
+
+  refs.openModalBtns.forEach((btn) => {
+    btn.addEventListener("click", toggleModal);
+  });
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 }
 
 function sendMail() {
   event.preventDefault();
-  document.getElementById("feedback-form");
 
   const formData = {
-    name: document.getElementById("feedback-name").value,
-    email: document.getElementById("feedback-mail").value,
-    message: document.getElementById("feedback-comment").value,
+    name: formInputs.name.value,
+    email: formInputs.email.value,
+    message: formInputs.message.value,
   };
 
   emailjs.send("service_djnyj9a", "template_li8ntcl", formData).then(
     function (response) {
       console.log("Feedback successfully send:", response);
-      alert("Feedback successfully send!");
-      refs.modal.classList.toggle("hidden");
+      if (formInputs.status.classList.contains("status-error")) {
+        formInputs.status.classList.remove("status-error");
+      }
+
+      if (!formInputs.status.classList.contains("status-ok")) {
+        formInputs.status.classList.add("status-ok");
+      }
+
+      clearFormInputs();
+      formInputs.status.textContent = "Feedback successfully send!";
     },
     function (error) {
       console.error("Feedback error send:", error);
-      alert("Feedback error send!");
+      if (formInputs.status.classList.contains("status-ok")) {
+        formInputs.status.classList.remove("status-ok");
+      }
+
+      if (!formInputs.status.classList.contains("status-error")) {
+        formInputs.status.classList.add("status-error");
+      }
+
+      formInputs.status.textContent = "Feedback error send!";
     }
   );
 }
+
+function formCustomValidation() {
+  formInputs.name.addEventListener("invalid", function () {
+    if (formInputs.name.value.length == 0) {
+      formInputs.name.setCustomValidity("This field cannot be empty");
+    } else {
+      formInputs.name.setCustomValidity("");
+    }
+  });
+
+  formInputs.email.addEventListener("invalid", function () {
+    if (formInputs.email.value.length == 0) {
+      formInputs.email.setCustomValidity("This field cannot be empty");
+    } else if (
+      !formInputs.email.validity.valid ||
+      formInputs.email.validity.typeMismatch
+    ) {
+      formInputs.email.setCustomValidity("Invalid e-mail address");
+    } else {
+      formInputs.email.setCustomValidity("");
+    }
+  });
+
+  formInputs.message.addEventListener("invalid", function () {
+    if (formInputs.message.value.length == 0) {
+      formInputs.message.setCustomValidity("This field cannot be empty");
+    } else {
+      formInputs.message.setCustomValidity("");
+    }
+  });
+
+  formInputs.name.addEventListener("input", function () {
+    formInputs.name.setCustomValidity("");
+  });
+
+  formInputs.email.addEventListener("input", function () {
+    if (formInputs.email.validity.typeMismatch) {
+      formInputs.email.setCustomValidity("Invalid e-mail address");
+    } else {
+      formInputs.email.setCustomValidity("");
+    }
+  });
+
+  formInputs.message.addEventListener("input", function () {
+    formInputs.message.setCustomValidity("");
+  });
+}
+
+formOpenCloseListeners();
+formCustomValidation();
